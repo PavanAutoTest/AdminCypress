04454a54b2373d99adfc9bc73bd72ecd1d7aa4ea
Vamsi <pavaneramalla@pandorarndlabs.com>
2024-01-24 13:16:19 +0530

    FifthCommit

--- cypress/e2e/all.cy.js
+++ cypress/e2e/all.cy.js
@@ -1,9 +1,9 @@
 import "./../../cypress/e2e/LoginPage.cy"
 //import "./API.cy"
-/* import "./../../cypress/e2e/UserCreation.cy" 
+import "./../../cypress/e2e/UserCreation.cy" 
 import "./../../cypress/e2e/HuntGroupCreation.cy"
 import "./../../cypress/e2e/AAGroupCreation.cy"
-import "./../../cypress/e2e/PhoneNumberValidation.cy" */
+import "./../../cypress/e2e/PhoneNumberValidation.cy" 
 
 
 
